e861fe00a17cdb9518d9aad13da8237d33b76423
Start Coding Student 1 <session1@START-Coding-MBP-10.local>
2015-10-26 15:27:05 -0700

    Updated photos used.

--- apps.js
+++ apps.js
@@ -1,40 +1,42 @@
 var main = function(){
+    var current1 = $('#one').attr('src');
     $('#one').click(function(){
-        $('#one').attr('src', 'http://41.media.tumblr.com/e7aa9b37eff85b01631302784a9ca245/tumblr_nkhpopzYjE1td9wezo1_500.jpg');
+        $('#one').attr('src', 'http://i.telegraph.co.uk/multimedia/archive/02707/syria_2707973b.jpg');
     });
 
     $('#two').click(function(){
-        $('#two').attr('src', 'http://40.media.tumblr.com/21cebdf85bee5a8d05808827be6b5eb6/tumblr_mn4pw7jISu1rdlwp2o1_500.jpg');
+        $('#two').attr('src', 'http://media.npr.org/assets/img/2012/01/26/syria_01_wide-b6c3b88b2988384753bccc0bad8cf08c8d062465-s900-c85.jpg');
     });
 
     $('#three').click(function(){
-        $('#three').attr('src', 'http://40.media.tumblr.com/d86715a2761a45f03be1a01867df37e1/tumblr_new5pmtAUT1sqeqqro1_500.jpg');
+        $('#three').attr('src', 'http://cdn.thewire.com/media/img/upload/wire/2014/02/12/RTX18EIW/lead_large.jpgg');
     });
 
     $('#four').click(function(){
-        $('#four').attr('src','http://40.media.tumblr.com/058b540f078bf900dabe9f482b52a097/tumblr_nu2ru6pohm1udn0cco1_500.jpg');
+        $('#four').attr('src','http://media.carbonated.tv/113976_story__around.JPG');
     });
 
     $('#five').click(function(){
-        $('#five').attr('src','http://40.media.tumblr.com/dc404a7a96317b48b0ad3bf2fde1230f/tumblr_nvb7wj0aM71sq77lyo1_500.jpg');
+        $('#five').attr('src','http://www.defendproclaimthefaith.org/blog/wp-content/uploads/mideast-syria-civil-war.jpg');
     });
 
     $('#six').click(function(){
-        $('#six').attr('src','http://36.media.tumblr.com/cf78964822e04c9a6f057c07c11e270d/tumblr_nqfx7vHskK1qkgmfvo1_500.jpg');
-    });
+        $('#six').attr('src','https://nationalpostnews.files.wordpress.com/2013/06/aleppo.jpg');
+});
 
     $('#seven').click(function(){
-        $('#seven').attr('src','http://40.media.tumblr.com/3816892a19564b8784711f7f7ef3ab77/tumblr_nsxr4uOo321qa4iv8o2_500.jpg');
+        $('#seven').attr('src','http://cdn.timesofisrael.com/uploads/2012/12/Mideast-Iraq_Horo3.jpg');
     });
 
     $('#eight').click(function(){
-        $('#eight').attr('src','http://40.media.tumblr.com/56ee9b971bc0acc2b6bd424c5df5a520/tumblr_nq56z8dWNR1qzs0wgo1_500.jpg');
+        $('#eight').attr('src','http://dailysignal.com/wp-content/uploads/150608_ISISSeflie_Johnson-1250x650.jpg');
     });
 
     $('#nine').click(function(){
-        $('#nine').attr('src','http://40.media.tumblr.com/27c81e870f7752ff82445c63f7c887f1/tumblr_nsxr4uOo321qa4iv8o4_500.png')
+        $('#nine').attr('http://i.ytimg.com/vi/WZh6jCLGwrg/maxresdefault.jpg')
     });
 
 };
 
 $(document).ready(main);
+
